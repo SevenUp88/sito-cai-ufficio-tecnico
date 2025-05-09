f2cd55d87c4438942c5cc6297e8ed5d6b9a1736b
SevenUp88 <sevendradi88@gmail.com>
2025-05-09 11:37:57 +0200

    Update script.js

--- LISTINI/CLIMA/multisplit/script.js
+++ LISTINI/CLIMA/multisplit/script.js
@@ -4,12 +4,6 @@
 document.addEventListener('DOMContentLoaded', async () => {
     console.log("DOM Contenuto Caricato - Inizio script.js (6-Step Flow - V2.1 Complete)");
 
-    // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
