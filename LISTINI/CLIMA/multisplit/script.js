d156172b675b8940341ae5748a0d3aaa1f390fd1
SevenUp88 <sevendradi88@gmail.com>
2025-05-14 17:21:16 +0200

    Update script.js

--- LISTINI/CLIMA/multisplit/script.js
+++ LISTINI/CLIMA/multisplit/script.js
@@ -604,7 +604,7 @@ document.addEventListener('DOMContentLoaded', async () => {
         newString = newString.replace(/</g, "<");
         newString = newString.replace(/>/g, ">");
         newString = newString.replace(/"/g, "'");
-        newString = newString.replace(/"/g, """);
+        newString = newString.replace(/"/g, "'");
         return newString;
     }
     function toggleAdminSectionVisibility() {
