3295d338d6058ce7755a69c4949ccdf0b3b377cf
SevenUp88 <sevendradi88@gmail.com>
2025-05-15 12:24:05 +0200

    Update script.js

--- LISTINI/CLIMA/multisplit/script.js
+++ LISTINI/CLIMA/multisplit/script.js
@@ -578,7 +578,7 @@ document.addEventListener('DOMContentLoaded', async () => {
         newString = newString.replace(/&/g, "&");
         newString = newString.replace(/</g, "<");
         newString = newString.replace(/>/g, ">");
-        newString = newString.replace(/"/g, """);
+        newString = newString.replace(/"/g, "'");
         newString = newString.replace(/'/g, "'");
         return newString;
     }
