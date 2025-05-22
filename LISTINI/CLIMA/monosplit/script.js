f858b6d5f98622c5d1cd606282030547fb9700ec
SevenUp88 <sevendradi88@gmail.com>
2025-05-22 09:24:03 +0200

    Update script.js

--- LISTINI/CLIMA/monosplit/script.js
+++ LISTINI/CLIMA/monosplit/script.js
@@ -1,15 +1,3 @@
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyC_gm-MK5dk2jc_MmmwO7TWBm7oW_D5t1Y",
-  authDomain: "consorzio-artigiani-idraulici.firebaseapp.com",
-  projectId: "consorzio-artigiani-idraulici",
-  storageBucket: "consorzio-artigiani-idraulici.firebasestorage.app",
-  messagingSenderId: "136848104008",
-  appId: "1:136848104008:web:2724f60607dbe91d09d67d",
-  measurementId: "G-NNPV2607G7"
-};
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
