16bc9b3f6065f18182bc8d646d564c23f3a6e8d4
SevenUp88 <sevendradi88@gmail.com>
2025-10-09 15:51:04 +0200

    Update gas-script.js

--- NOLEGGI/GAS/gas-script.js
+++ NOLEGGI/GAS/gas-script.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const db = firebase.firestore();
     
-    const GOOGLE_APPS_SCRIPT_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbyyBeWhl1rH4flw59NVvNyIFYoVE0cDFlqRfcd0SVWKKAAh4mo0nfJ-O009FpIfUljT/exec'; 
+    const GOOGLE_APPS_SCRIPT_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbxAkcbFNgQ35ds2kw05pBsFKpnQsfp4uYYQKaHwFeKTFez-mMg6rW61s_0Im3AHmLao/exec'; 
     
     const FIRESTORE_COLLECTION_NAME = 'gasCylinders';
     const GOOGLE_SHEET_NAME = 'gas';
